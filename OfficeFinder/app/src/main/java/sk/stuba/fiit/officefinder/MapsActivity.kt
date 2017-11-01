905a3e0d05c7ff3890d8d200a5a9dd57c96d3624
blaskoa <adam.blasko1@gmail.com>
2017-11-01 20:19:52 +0100

    Initial commit for both backend and frontend

---
+++ OfficeFinder/app/src/main/java/sk/stuba/fiit/officefinder/MapsActivity.kt
@@ -0,0 +1,51 @@
+package sk.stuba.fiit.officefinder
+
+import android.os.Bundle
+import android.support.v4.app.FragmentActivity
+import android.util.Log
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import com.google.maps.android.data.geojson.GeoJsonLayer
+
+class MapsActivity : FragmentActivity(), OnMapReadyCallback {
+
+    private var map: GoogleMap? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_maps)
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        val mapFragment = supportFragmentManager
+                .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    override fun onMapReady(googleMap: GoogleMap) {
+        map = googleMap
+
+        // Add a marker in Sydney and move the
+
+        val test = GeoJsonLayer(map, R.raw.geo, applicationContext)
+
+        test.setOnFeatureClickListener { feature -> Log.d("asd", feature.getProperty("name")) }
+        test.addLayerToMap()
+        val bratislava = LatLng(10.0, 125.6)
+        map!!.addMarker(MarkerOptions().position(bratislava).title("Marker in Bratislava"))
+        map!!.moveCamera(CameraUpdateFactory.newLatLng(bratislava))
+        map!!.moveCamera(CameraUpdateFactory.zoomTo(15F))
+    }
+}
---
+++ OfficeFinderBackend/OfficeFinderBackend/Controllers/OfficesController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace OfficeFinderBackend.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Offices")]
+    public class OfficesController : Controller
+    {
+        // GET: api/Offices
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new[] { "value1", "value2" };
+        }
+
+        // GET: api/Offices/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+        
+        // POST: api/Offices
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+        
+        // PUT: api/Offices/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+        
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
